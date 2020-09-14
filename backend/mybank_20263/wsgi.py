7d47c8ee32196f89983cfe216603799878a61ff1
Crowdbotics <team@crowdbotics.com>
2020-09-14 16:35:23 +0000

    Initial commit.

---
+++ backend/mybank_20263/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mybank_20263 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mybank_20263.settings')
+
+application = get_wsgi_application()
